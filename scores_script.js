213c5f4d08447872d524cd8e49b7fe1782ff94d6
Christian Sinai <christian_sinai@me.com>
2020-08-13 11:41:52 -0700

    added submit results funtionality

--- scores_script.js
+++ scores_script.js
@@ -1,18 +1,61 @@
-// scores variables ...
-var results = document.querySelector("#results");
-var scores = []
+// score variables ...
+var initialsInput = document.querySelector("#initials-text");
+var resultsList = document.querySelector("#results-list");
+console.log(resultsList);
+
+var results = [];
 
 function renderResults() {
-    // Clear results element
-    results.innerHTML = "";
-  
-    // Render a new li for each result
-    for (var i = 0; i < scores.length; i++) {
-      var score = scores[i];
+  // Clear results element
+  resultsList.innerHTML = "";
+
+  // Render a new li for each result
+  for (var i = 0; i < results.length; i++) {
+      var result = results[i];
   
       var li = document.createElement("li");
-      li.textContent = score;
-      li.setAttribute("score-index", i);
-    }
+      li.textContent = result;
+      li.setAttribute("result-index", i);
+
+      resultsList.appendChild(li);
   }
-  
+}
+
+function init() {
+  // Get stored results from localStorage
+  // Parsing the JSON string to an object
+  var storedResults = JSON.parse(localStorage.getItem("results"));
+
+  // If results were retrieved from localStorage, update the results array to it
+  if (storedResults !== null) {
+      results = storedResults;
+  }
+
+  // Render results to the DOM
+  renderResults();
+}
+
+function storeResults() {
+  // Stringify and set "results" key in localStorage to results array
+  localStorage.setItem("results", JSON.stringify(results));
+}
+
+// When a result is submitted ...
+document.getElementById("submit").addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var resultText = initialsInput.value.trim();
+
+  // Return from function early if submitted resultText is blank
+  if (resultText === "") {
+      return;
+  }
+
+  // Add new resultText to results array, clear the input
+  results.push(resultText);
+  initialsInput.value = "";
+
+  // Store updated results in localStorage, re-render the list
+  storeResults();
+  renderResults();
+});
--- script.js
+++ script.js
@@ -6,12 +6,21 @@ var secondsLeft = 60;
 // main variables ...
 var startQuiz = document.querySelector("#start");
 var startBttn = document.querySelector("#start-button");
+
 var q1Div = document.querySelector("#q1");
 var q2Div = document.querySelector("#q2");
 var q3Div = document.querySelector("#q3");
 var q4Div = document.querySelector("#q4");
 var q5Div = document.querySelector("#q5");
+
 var done = document.querySelector("#done");
+
+// score variables ...
+// var initialsInput = document.querySelector("#initials-text");
+// var resultsList = document.querySelector("#results-list");
+// console.log(resultsList);
+
+// var results = [];
 
 // countdown timer initial set point ...
 timerEl.textContent = "Time: 0";
@@ -93,9 +102,65 @@ document.getElementById("q5").addEventListener("click", function(event) {
     }
     if(event.target.matches("button")) {
         clearInterval(timerInterval);
+        timerEl.textContent = "Time: 0";
         q5Div.classList.add("hide");
         done.classList.remove("hide");
         // console.log(secondsLeft);
         document.getElementById("score").innerHTML = secondsLeft;
     }
 });
+
+// function renderResults() {
+//     // Clear results element
+//     resultsList.innerHTML = "";
+
+//     // Render a new li for each result
+//     for (var i = 0; i < results.length; i++) {
+//         var result = results[i];
+    
+//         var li = document.createElement("li");
+//         li.textContent = result;
+//         li.setAttribute("result-index", i);
+
+//         resultsList.appendChild(li);
+//     }
+// }
+
+// function init() {
+//     // Get stored results from localStorage
+//     // Parsing the JSON string to an object
+//     var storedResults = JSON.parse(localStorage.getItem("results"));
+
+//     // If results were retrieved from localStorage, update the results array to it
+//     if (storedResults !== null) {
+//         results = storedResults;
+//     }
+
+//     // Render results to the DOM
+//     renderResults();
+// }
+
+// function storeResults() {
+//     // Stringify and set "results" key in localStorage to results array
+//     localStorage.setItem("results", JSON.stringify(results));
+// }
+
+// // When a result is submitted ...
+// document.getElementById("submit").addEventListener("click", function(event) {
+//     event.preventDefault();
+
+//     var resultText = initialsInput.value.trim();
+
+//     // Return from function early if submitted resultText is blank
+//     if (resultText === "") {
+//         return;
+//     }
+
+//     // Add new resultText to results array, clear the input
+//     results.push(resultText);
+//     initialsInput.value = "";
+
+//     // Store updated results in localStorage, re-render the list
+//     storeResults();
+//     renderResults();
+// });
